ba889450b360f873eb6e70100d6c2bcc3061eba4
Aaron Andy <ayay@ayays-MacBook-Pro.local>
2025-10-03 15:21:09 -0400

    add http gem

--- Gemfile
+++ Gemfile
@@ -64,9 +64,8 @@ end
 
 gem "httparty", "~> 0.23.1"
 gem 'devise'
-# OmniAuth core and the specific Strava strategy
 gem 'omniauth'
 gem 'omniauth-strava'
-# OmniAuth security patch for Rails
 gem 'omniauth-rails_csrf_protection'
 gem "multi_json", "~> 1.17.0"
+gem "http"
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require "http"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,6 +20,7 @@ module BicyclingBackend
     # Configuration for the application, engines, and railties goes here.
 
     config.autoload_paths << Rails.root.join('app', 'services')
+    config.eager_load_paths << Rails.root.join('app', 'services')
 
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
